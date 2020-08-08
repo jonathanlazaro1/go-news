3cd8ca40e0700502b7027fcc6869e5d04d743103
Jonathan Lazaro <jonathan.lazaro1@gmail.com>
2020-08-08 14:59:16 -0400

    feat: add endpoint to handle search

--- main.go
+++ main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"text/template"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, _ *http.Request) {
 	tpl.Execute(w, nil)
+}
+
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
+	}
+	params := u.Query()
+	searchKey := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is:", searchKey)
+	fmt.Println("Results page is:", page)
 }
 
 func main() {
@@ -23,6 +43,7 @@ func main() {
 	fs := http.FileServer(http.Dir("assets"))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
+	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
